3a09f34596ea3bfc9791e0960f5e8754065874e1
Petter Daae <petter.daae@bekk.no>
2023-12-07 18:26:40 +0100

    Terraform setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# arithmetic-lambda
+
+## Terraform
+
+The `/terraform` folder contains the necessary infrastructure. It is also used to deploy new versions of the lambda.
+
---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.6.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.29.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "674283423467-terraform-state"
+    key    = "arithmetic-lambda"
+    region = "eu-north-1"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["674283423467"]
+  region              = "us-east-1"
+}
