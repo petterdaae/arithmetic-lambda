92e5ffa4742f4508182b855bb35e9484de20695e
Petter Daae <petter.daae@bekk.no>
2023-12-07 20:16:10 +0100

    First implementation of arithmetic operations

--- index.js
+++ index.js
@@ -6,9 +6,48 @@ function parseBody(event) {
   );
 }
 
+function clientError(message) {
+  return {
+    statusCode: 400,
+    body: {
+      message
+    }
+  }
+}
+
 module.exports.handler = async (event, context) => {
-  const method = event.requestContext.http.method;
   const body = parseBody(event);
-  console.log(method, body);
-  return "Hello world!\n"
+
+  const operation = body.operation;
+  const left = body.left;
+  const right = body.right;
+
+  let result = null;
+
+  switch (operation) {
+    case "ADD":
+      result = left + right
+      break
+
+    case "SUBTRACT":
+      result = left - right
+      break
+
+    case "MULTIPLY":
+      result = left * right
+      break
+
+    case "DIVIDE":
+      result = left / right
+      break
+
+    default:
+      return clientError("Invalid operation")
+
+  }
+
+  return {
+    request: { operation, left, right },
+    result
+  }
 }
