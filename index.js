cc573c7adbfc6e4ef0569e053b7538032f61f98a
Petter Daae <petter.daae@bekk.no>
2023-12-07 20:02:35 +0100

    Get http method and body from lambda event

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function parseBody(event) {
+  return JSON.parse(
+    event.isBase64Encoded
+      ? atob(event.body)
+      : event.body
+  );
+}
+
 module.exports.handler = async (event, context) => {
+  const method = event.requestContext.http.method;
+  const body = parseBody(event);
+  console.log(method, body);
   return "Hello world!\n"
 }
