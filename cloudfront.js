f89b35c016c43f9127a752869260d8ed40760725
Petter Daae <petter.daae@bekk.no>
2023-12-08 15:39:10 +0100

    Add arithmetic to cloudfront function

--- cloudfront.js
+++ cloudfront.js
@@ -1,6 +1,49 @@
+function clientError(message) {
+  return {
+    statusCode: 400,
+    body: message
+  }
+}
+
 function handler(event) {
+  try {
+    var operation = event.request.querystring.operation.value;
+    var left = parseFloat(event.request.querystring.left.value);
+    var right = parseFloat(event.request.querystring.right.value);
+
+    var result;
+    switch (operation) {
+      case "ADD":
+        result = left + right;
+        break;
+
+      case "SUBTRACT":
+        result = left - right;
+        break;
+
+      case "MULTIPLY":
+        result = left * right;
+        break;
+
+      case "DIVIDE":
+        result = left / right;
+        break;
+
+      default:
+        return clientError("Invalid operation")
+    }
+
+  } catch (e) {
+    return clientError("Invalid request")
+  }
+
   return {
     statusCode: 200,
-    body: "Hello world from CloudFront function!"
+    body: {
+      encoding: "text",
+      data: JSON.stringify({
+        result
+      })
+    },
   }
 }
