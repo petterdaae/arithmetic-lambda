c64850c5003b00f76a18e316277a6ac122b8526a
Petter Daae <petter.daae@bekk.no>
2023-12-08 15:41:18 +0100

    Remove event validation

--- lambda.js
+++ lambda.js
@@ -15,47 +15,38 @@ function clientError(message) {
   }
 }
 
-function validEvent(event) {
-  return event
-    && event.body
-    && event.method == "POST"
-    && typeof event.body.operation === 'string'
-    && typeof event.body.left === 'number'
-    && typeof event.body.right === 'number';
-}
-
 module.exports.handler = async (event, context) => {
-  if (!validEvent(event)) {
-    return clientError("Invalid request");
-  }
+  try {
+    const body = parseBody(event);
 
-  const body = parseBody(event);
+    const operation = body.operation;
+    const left = body.left;
+    const right = body.right;
 
-  const operation = body.operation;
-  const left = body.left;
-  const right = body.right;
+    let result = null;
 
-  let result = null;
+    switch (operation) {
+      case "ADD":
+        result = left + right
+        break
 
-  switch (operation) {
-    case "ADD":
-      result = left + right
-      break
+      case "SUBTRACT":
+        result = left - right
+        break
 
-    case "SUBTRACT":
-      result = left - right
-      break
+      case "MULTIPLY":
+        result = left * right
+        break
 
-    case "MULTIPLY":
-      result = left * right
-      break
+      case "DIVIDE":
+        result = left / right
+        break
 
-    case "DIVIDE":
-      result = left / right
-      break
-
-    default:
-      return clientError("Invalid operation")
+      default:
+        return clientError("Invalid operation")
+    }
+  } catch (e) {
+    return clientError("Invalid request")
   }
 
   return {
